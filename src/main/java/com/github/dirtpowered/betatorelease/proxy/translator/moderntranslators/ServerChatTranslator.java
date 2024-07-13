1f8120078646e04b593d41603877710f647ac804
outofmem13 <nulln785@gmail.com>
2024-07-13 16:03:36 +0200

    fix chat colors on new lines, disconnect messages

--- src/main/java/com/github/dirtpowered/betatorelease/proxy/translator/moderntranslators/ServerChatTranslator.java
+++ src/main/java/com/github/dirtpowered/betatorelease/proxy/translator/moderntranslators/ServerChatTranslator.java
@@ -4,10 +4,13 @@ import com.github.dirtpowered.betatorelease.network.session.Session;
 import com.github.dirtpowered.betatorelease.proxy.translator.ModernToBetaHandler;
 import com.github.steveice10.mc.protocol.data.game.MessageType;
 import com.github.steveice10.mc.protocol.packet.ingame.server.ServerChatPacket;
+import net.md_5.bungee.api.ChatColor;
 import net.md_5.bungee.api.chat.TextComponent;
 import net.md_5.bungee.chat.ComponentSerializer;
 
 public class ServerChatTranslator implements ModernToBetaHandler<ServerChatPacket> {
+    private static final int MAX_PACKET_LENGTH = 100;
+    private static final int LINE_LENGTH = 64;
 
     @Override
     public void translate(ServerChatPacket packet, Session betaSession) {
@@ -17,18 +20,73 @@ public class ServerChatTranslator implements ModernToBetaHandler<ServerChatPacke
         String messageJson = packet.getMessage().toJsonString();
         String formattedMessage = TextComponent.toLegacyText(ComponentSerializer.parse(messageJson));
 
-        // we need to split the message if it's too long
-        if (formattedMessage.length() > 100) {
-            int index = 0;
-            while (index < formattedMessage.length()) {
-                int endIndex = Math.min(index + 100, formattedMessage.length());
-                String part = formattedMessage.substring(index, endIndex);
-                betaSession.sendMessage(part);
-                index = endIndex;
-            }
+        if (formattedMessage.length() <= MAX_PACKET_LENGTH) {
+            sendFormattedMessage(formattedMessage, betaSession);
             return;
         }
 
-        betaSession.sendMessage(formattedMessage);
+        int index = 0;
+        String lastColorCode = "";
+
+        while (index < formattedMessage.length()) {
+            int endIndex = Math.min(index + MAX_PACKET_LENGTH, formattedMessage.length());
+            String part = formattedMessage.substring(index, endIndex);
+            StringBuilder parts = new StringBuilder();
+
+            int i = 0;
+            while (i < part.length()) {
+                int lineEndIndex = Math.min(i + LINE_LENGTH, part.length());
+
+                if (lineEndIndex < part.length() && part.charAt(lineEndIndex - 1) == ChatColor.COLOR_CHAR)
+                    lineEndIndex--;
+
+                String linePart = part.substring(i, lineEndIndex);
+
+                if (!lastColorCode.isEmpty())
+                    linePart = lastColorCode + linePart;
+
+                parts.append(linePart);
+                betaSession.sendMessage(parts.toString());
+                parts.setLength(0); // reset
+
+                lastColorCode = getLastColorCode(linePart);
+                i = lineEndIndex;
+            }
+            index = endIndex;
+        }
+    }
+
+    private void sendFormattedMessage(String message, Session betaSession) {
+        String lastColorCode = "";
+        int index = 0;
+
+        while (index < message.length()) {
+            int endIndex = Math.min(index + ServerChatTranslator.LINE_LENGTH, message.length());
+
+            if (endIndex < message.length() && message.charAt(endIndex - 1) == ChatColor.COLOR_CHAR)
+                endIndex--;
+
+            String part = message.substring(index, endIndex);
+            if (!lastColorCode.isEmpty())
+                part = lastColorCode + part;
+
+            betaSession.sendMessage(part);
+
+            lastColorCode = getLastColorCode(part);
+            index = endIndex;
+        }
+    }
+
+    private String getLastColorCode(String text) {
+        String lastColorCode = "";
+        int length = text.length();
+
+        for (int i = 0; i < length - 1; i++) {
+            if (text.charAt(i) == ChatColor.COLOR_CHAR) {
+                lastColorCode = text.substring(i, i + 2);
+                i++;
+            }
+        }
+        return lastColorCode;
     }
 }
--- src/main/java/com/github/dirtpowered/betatorelease/proxy/translator/moderntranslators/ServerDisconnectTranslator.java
+++ src/main/java/com/github/dirtpowered/betatorelease/proxy/translator/moderntranslators/ServerDisconnectTranslator.java
@@ -3,16 +3,17 @@ package com.github.dirtpowered.betatorelease.proxy.translator.moderntranslators;
 import com.github.dirtpowered.betatorelease.network.session.Session;
 import com.github.dirtpowered.betatorelease.proxy.translator.ModernToBetaHandler;
 import com.github.steveice10.mc.protocol.packet.ingame.server.ServerDisconnectPacket;
-import net.md_5.bungee.api.chat.TextComponent;
-import net.md_5.bungee.chat.ComponentSerializer;
 
 public class ServerDisconnectTranslator implements ModernToBetaHandler<ServerDisconnectPacket> {
 
     @Override
     public void translate(ServerDisconnectPacket packet, Session betaSession) {
         String reason = packet.getReason().getFullText();
-        String formattedMessage = TextComponent.toLegacyText(ComponentSerializer.parse(reason));
 
-        betaSession.disconnect(formattedMessage);
+        // the beta client is capable of displaying only up to 100 characters
+        if (reason.length() > 100)
+            reason = reason.substring(0, 100);
+
+        betaSession.disconnect(reason);
     }
 }
