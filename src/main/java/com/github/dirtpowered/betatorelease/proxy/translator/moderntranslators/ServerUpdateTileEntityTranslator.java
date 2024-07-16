4c299297d04e6a1533aa01b7c3250e0dd22f1afa
outofmem13 <nulln785@gmail.com>
2024-07-16 23:17:20 +0200

    fix signs

--- src/main/java/com/github/dirtpowered/betatorelease/proxy/translator/moderntranslators/ServerUpdateTileEntityTranslator.java
+++ src/main/java/com/github/dirtpowered/betatorelease/proxy/translator/moderntranslators/ServerUpdateTileEntityTranslator.java
@@ -1,17 +1,15 @@
 package com.github.dirtpowered.betatorelease.proxy.translator.moderntranslators;
 
-import com.github.dirtpowered.betaprotocollib.packet.Version_B1_7.data.BlockChangePacketData;
 import com.github.dirtpowered.betaprotocollib.packet.Version_B1_7.data.UpdateSignPacketData;
-import com.github.dirtpowered.betatorelease.utils.Utils;
 import com.github.dirtpowered.betatorelease.network.session.Session;
 import com.github.dirtpowered.betatorelease.proxy.translator.ModernToBetaHandler;
 import com.github.steveice10.mc.protocol.data.game.entity.metadata.Position;
 import com.github.steveice10.mc.protocol.data.game.world.block.UpdatedTileType;
 import com.github.steveice10.mc.protocol.packet.ingame.server.world.ServerUpdateTileEntityPacket;
 import com.github.steveice10.opennbt.tag.builtin.CompoundTag;
-import com.github.steveice10.opennbt.tag.builtin.StringTag;
 import com.github.steveice10.opennbt.tag.builtin.Tag;
-import org.pmw.tinylog.Logger;
+import net.md_5.bungee.api.chat.TextComponent;
+import net.md_5.bungee.chat.ComponentSerializer;
 
 import java.util.Map;
 
@@ -33,21 +31,12 @@ public class ServerUpdateTileEntityTranslator implements ModernToBetaHandler<Ser
         String[] signLines = new String[4];
 
         for (int line = 0; line < 4; ++line) {
-            signLines[line] = Utils.getSignText((StringTag) strings.get("Text" + (line + 1)));
+            try {
+                signLines[line] = TextComponent.toPlainText(ComponentSerializer.parse(strings.get("Text" + (line + 1)).getValue() + ""));
+            } catch (Exception e) {
+                signLines[line] = "error";
+            }
         }
-
-        Logger.info("------- SIGN TEXT -------");
-        for (int i = 0, signLinesLength = signLines.length; i < signLinesLength; i++) {
-            String signLine = signLines[i];
-            Logger.info("Text{}: {}", i, signLine);
-        }
-        Logger.info("------- SIGN END -------");
-
-        /*
-         * https://wiki.vg/index.php?title=Protocol&oldid=14204#Open_Sign_Editor
-         * There must already be a sign at the given location (which the client does not do automatically) - send a Block Change first.
-         */
-        betaSession.sendPacket(new BlockChangePacketData(x, y, z, 63 /* working somehow also on wall-sign */, 0));
         betaSession.sendPacket(new UpdateSignPacketData(x, y, z, new String[]{signLines[0], signLines[1], signLines[2], signLines[3]}));
     }
 }
