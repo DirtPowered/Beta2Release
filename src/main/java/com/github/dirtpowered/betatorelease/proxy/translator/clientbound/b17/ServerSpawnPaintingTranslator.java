3156d6a9c8b89123bf5fa15251a9617e4dd21ca7
outofmem13 <nulln785@gmail.com>
2024-07-22 00:40:48 +0200

    fix signs, this time for sure & fix painting crash

--- src/main/java/com/github/dirtpowered/betatorelease/proxy/translator/clientbound/b17/ServerSpawnPaintingTranslator.java
+++ src/main/java/com/github/dirtpowered/betatorelease/proxy/translator/clientbound/b17/ServerSpawnPaintingTranslator.java
@@ -5,6 +5,7 @@ import com.github.dirtpowered.betatorelease.network.session.Session;
 import com.github.dirtpowered.betatorelease.proxy.translator.ModernToBetaHandler;
 import com.github.steveice10.mc.protocol.data.MagicValues;
 import com.github.steveice10.mc.protocol.data.game.entity.metadata.Position;
+import com.github.steveice10.mc.protocol.data.game.entity.type.PaintingType;
 import com.github.steveice10.mc.protocol.packet.ingame.server.entity.spawn.ServerSpawnPaintingPacket;
 
 public class ServerSpawnPaintingTranslator implements ModernToBetaHandler<ServerSpawnPaintingPacket> {
@@ -13,9 +14,13 @@ public class ServerSpawnPaintingTranslator implements ModernToBetaHandler<Server
     public void translate(ServerSpawnPaintingPacket packet, Session betaSession) {
         int entityId = packet.getEntityId();
         Position position = packet.getPosition();
+        PaintingType type = packet.getPaintingType();
+
+        // Wither painting is the only one that doesn't exist in beta 1.7
+        if (type == PaintingType.WITHER) type = PaintingType.KEBAB;
 
         int direction = MagicValues.value(Integer.class, packet.getDirection());
-        String title = MagicValues.value(String.class, packet.getPaintingType());
+        String title = MagicValues.value(String.class, type);
 
         int x = position.getX();
         int y = position.getY();
--- src/main/java/com/github/dirtpowered/betatorelease/utils/Utils.java
+++ src/main/java/com/github/dirtpowered/betatorelease/utils/Utils.java
@@ -7,6 +7,8 @@ import com.github.dirtpowered.betatorelease.network.session.Session;
 import com.github.steveice10.mc.protocol.data.game.entity.metadata.ItemStack;
 import com.github.steveice10.opennbt.tag.builtin.CompoundTag;
 import com.github.steveice10.opennbt.tag.builtin.Tag;
+import net.md_5.bungee.api.ChatColor;
+import net.md_5.bungee.api.chat.BaseComponent;
 import net.md_5.bungee.api.chat.TextComponent;
 import net.md_5.bungee.chat.ComponentSerializer;
 import org.apache.commons.lang3.StringUtils;
@@ -46,13 +48,28 @@ public class Utils {
 
         for (int line = 0; line < 4; ++line) {
             try {
-                String legacy = TextComponent.toLegacyText(ComponentSerializer.parse(strings.get("Text" + (line + 1)).getValue() + ""));
-                signLines[line] = StringUtils.substring(legacy, 0, 15);
+                BaseComponent[] components = ComponentSerializer.parse(strings.get("Text" + (line + 1)).getValue() + "");
+                for (BaseComponent component : components) {
+                    fixSignComponent(component);
+                }
+                String legacy = TextComponent.toLegacyText(components);
+                signLines[line] = StringUtils.substring(legacy.replaceAll("§k", ""), 0, 15);
             } catch (Exception e) {
                 signLines[line] = "error";
             }
         }
         return signLines;
+    }
+
+    private static void fixSignComponent(BaseComponent component) {
+        // Bungee chat is shit. We'll find a better workaround later
+        if (component.getColorRaw() == null)
+            component.setColor(ChatColor.MAGIC);
+
+        component.setBold(false);
+        component.setItalic(false);
+        component.setUnderlined(false);
+        component.setStrikethrough(false);
     }
 
     public static void updateInventory(Session session, BetaItemStack currentItem) {
