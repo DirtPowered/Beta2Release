7796de289719b96071c8d3aed5d9471db84dcb12
outofmem13 <nulln785@gmail.com>
2024-09-13 03:06:29 +0200

    fix config loading & prepare for release

--- src/main/java/com/github/dirtpowered/betatorelease/configuration/Configuration.java
+++ src/main/java/com/github/dirtpowered/betatorelease/configuration/Configuration.java
@@ -3,6 +3,9 @@ package com.github.dirtpowered.betatorelease.configuration;
 import com.electronwill.nightconfig.core.file.CommentedFileConfig;
 import lombok.Getter;
 
+import java.nio.file.Files;
+import java.nio.file.Paths;
+
 @Getter
 public class Configuration {
     private final String bindAddress;
@@ -10,21 +13,24 @@ public class Configuration {
     private final String remoteAddress;
     private final int remotePort;
 
+    private static final String CONFIG_FILE_PATH = "config.toml";
+
     public Configuration() {
-        CommentedFileConfig config = CommentedFileConfig.builder("config.toml").build();
-        if (!config.getFile().exists()) {
-
+        CommentedFileConfig config = CommentedFileConfig.builder(CONFIG_FILE_PATH).build();
+        if (!Files.exists(Paths.get(CONFIG_FILE_PATH))) {
+            try {
+                config.load();
+                config.set("bind-address", "127.0.0.1");
+                config.set("bind-port", 25565);
+                config.set("remote-address", "127.0.0.1");
+                config.set("remote-port", 25566);
+                config.save();
+            } catch (Exception e) {
+                throw new RuntimeException("Failed to create or initialize the config file", e);
+            }
+        } else {
             config.load();
-
-            config.set("bind-address", "127.0.0.1");
-            config.set("bind-port", 25565);
-
-            config.set("remote-address", "127.0.0.1");
-            config.set("remote-port", 25566);
-            config.save();
         }
-
-        config.load();
 
         this.bindAddress = config.get("bind-address");
         this.bindPort = config.get("bind-port");
