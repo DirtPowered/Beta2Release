b4b90c72f94209cf1051ba46e5b8e90a2e8ab5fd
outofmem13 <nulln785@gmail.com>
2024-09-18 01:17:02 +0200

    finish map translator

--- src/main/java/com/github/dirtpowered/betatorelease/proxy/translator/clientbound/ServerMapDataTranslator.java
+++ src/main/java/com/github/dirtpowered/betatorelease/proxy/translator/clientbound/ServerMapDataTranslator.java
@@ -4,6 +4,8 @@ import com.github.dirtpowered.betaprotocollib.packet.Version_B1_7.data.V1_7_3Map
 import com.github.dirtpowered.betatorelease.network.session.Session;
 import com.github.dirtpowered.betatorelease.proxy.translator.ModernToBetaHandler;
 import com.github.steveice10.mc.protocol.data.game.world.map.MapData;
+import com.github.steveice10.mc.protocol.data.game.world.map.MapIcon;
+import com.github.steveice10.mc.protocol.data.game.world.map.MapIconType;
 import com.github.steveice10.mc.protocol.packet.ingame.server.world.ServerMapDataPacket;
 
 public class ServerMapDataTranslator implements ModernToBetaHandler<ServerMapDataPacket> {
@@ -12,22 +14,46 @@ public class ServerMapDataTranslator implements ModernToBetaHandler<ServerMapDat
     public void translate(ServerMapDataPacket packet, Session betaSession) {
         MapData data = packet.getData();
 
-        if (data == null || data.getRows() != 128 || data.getColumns() != 128)
-            return; // skip invalid data which can't be translated 1:1
+        if (data == null || data.getRows() != 128 || data.getColumns() != 128) {
+            MapIcon[] icons = packet.getIcons();
+            byte[] iconData = new byte[icons.length * 3 + 1]; // 3 bytes per icon + 1 byte for header
+            iconData[0] = 1; // show icons
 
-        // TODO: cursors, icons, etc
-        byte[] a = data.getData();
+            for (int i = 0; i < icons.length; i++) {
+                MapIcon icon = icons[i];
+                byte direction = (byte) icon.getIconRotation();
+
+                iconData[i * 3 + 1] = (byte) ((direction << 4) | toLegacyIconType(icon.getIconType()));
+                iconData[i * 3 + 2] = (byte) icon.getCenterX();
+                iconData[i * 3 + 3] = (byte) icon.getCenterZ();
+            }
+            betaSession.sendPacket(new V1_7_3MapDataPacketData(packet.getMapId(), iconData));
+            return;
+        }
+
+        byte[] mapData = data.getData();
 
         for (int x = 0; x < 128; ++x) {
-            byte[] bytes = new byte[131];
-            bytes[1] = (byte) x;
+            byte[] columnData = new byte[131]; // 128 map pixels + 3 header bytes
+            columnData[1] = (byte) x;
 
             for (int y = 0; y < 128; ++y) {
-                byte color = a[y * 128 + x];
-                bytes[y + 3] = fixColor(color);
+                byte color = mapData[y * 128 + x];
+                columnData[y + 3] = fixColor(color);
             }
-            betaSession.sendPacket(new V1_7_3MapDataPacketData(packet.getMapId(), bytes));
+            betaSession.sendPacket(new V1_7_3MapDataPacketData(packet.getMapId(), columnData));
         }
+    }
+
+
+    private byte toLegacyIconType(MapIconType iconType) {
+        return switch (iconType) {
+            case GREEN_ARROW -> 1;
+            case RED_ARROW, TEMPLE, MANSION, RED_POINTER -> 2;
+            case BLUE_ARROW -> 3;
+            case WHITE_CROSS -> 4;
+            default -> 0;
+        };
     }
 
     private byte fixColor(byte color) {
