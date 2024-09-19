c12c6933f50c32846b30dd465ad1a0b0e0f9c52a
outofmem13 <nulln785@gmail.com>
2024-09-19 02:00:43 +0200

    add support for playing discs in jukeboxes & update mappings

--- src/main/java/com/github/dirtpowered/betatorelease/proxy/translator/clientbound/ServerPlayEffectTranslator.java
+++ src/main/java/com/github/dirtpowered/betatorelease/proxy/translator/clientbound/ServerPlayEffectTranslator.java
@@ -1,12 +1,15 @@
 package com.github.dirtpowered.betatorelease.proxy.translator.clientbound;
 
 import com.github.dirtpowered.betaprotocollib.packet.Version_B1_7.data.V1_7_3SoundEffectPacketData;
+import com.github.dirtpowered.betatorelease.Main;
 import com.github.dirtpowered.betatorelease.data.remap.BlockMappings;
 import com.github.dirtpowered.betatorelease.network.session.Session;
 import com.github.dirtpowered.betatorelease.proxy.translator.ModernToBetaHandler;
 import com.github.steveice10.mc.protocol.data.game.entity.metadata.Position;
 import com.github.steveice10.mc.protocol.data.game.world.effect.BreakBlockEffectData;
 import com.github.steveice10.mc.protocol.data.game.world.effect.ParticleEffect;
+import com.github.steveice10.mc.protocol.data.game.world.effect.RecordEffectData;
+import com.github.steveice10.mc.protocol.data.game.world.effect.SoundEffect;
 import com.github.steveice10.mc.protocol.packet.ingame.server.world.ServerPlayEffectPacket;
 
 public class ServerPlayEffectTranslator implements ModernToBetaHandler<ServerPlayEffectPacket> {
@@ -21,6 +24,20 @@ public class ServerPlayEffectTranslator implements ModernToBetaHandler<ServerPla
             betaSession.sendPacket(new V1_7_3SoundEffectPacketData(2001, position.getX(), position.getY(), position.getZ(), packedData));
         } else if (packet.getEffect() == ParticleEffect.SMOKE) {
             betaSession.sendPacket(new V1_7_3SoundEffectPacketData(2000, position.getX(), position.getY(), position.getZ(), 0));
+        }
+
+        // handle jukebox discs
+        if (packet.getEffect() == SoundEffect.RECORD && packet.getData() instanceof RecordEffectData data) {
+            int recordId = data.getRecordId();
+
+            if (recordId != 2256 && recordId != 2257) {
+                Main.LOGGER.warn("Unknown record id: " + recordId);
+                return;
+            }
+            betaSession.sendPacket(new V1_7_3SoundEffectPacketData(1005, position.getX(), position.getY(), position.getZ(), recordId));
+        } else if (packet.getEffect() == SoundEffect.BLOCK_IRON_DOOR_CLOSE) {
+            // mcprotocollib has wrong mapping for this sound
+            betaSession.sendPacket(new V1_7_3SoundEffectPacketData(1005, position.getX(), position.getY(), position.getZ(), 0));
         }
     }
 }
