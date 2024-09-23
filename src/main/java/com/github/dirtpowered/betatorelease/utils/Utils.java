221e5492b48586aa0063f702ec047f4bb3f63edb
outofmem13 <nulln785@gmail.com>
2024-09-23 11:06:48 +0200

    fix small movement inconsistencies

--- src/main/java/com/github/dirtpowered/betatorelease/utils/Utils.java
+++ src/main/java/com/github/dirtpowered/betatorelease/utils/Utils.java
@@ -84,12 +84,7 @@ public class Utils {
     }
 
     public static int toAbsolutePos(double pos) {
-        return floor_double(pos * 32.0D);
-    }
-
-    private static int floor_double(double var0) {
-        int var2 = (int) var0;
-        return var0 < (double) var2 ? var2 - 1 : var2;
+        return (int) (pos * 32.0D);
     }
 
     private static int floor_float(float var0) {
