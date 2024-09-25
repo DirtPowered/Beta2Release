f37006f8bddc149efa7bf5b391bdc0236698ee2a
outofmem13 <nulln785@gmail.com>
2024-09-25 13:28:37 +0200

    fix lighting bolt entity position

--- src/main/java/com/github/dirtpowered/betatorelease/proxy/translator/clientbound/ServerSpawnGlobalEntityTranslator.java
+++ src/main/java/com/github/dirtpowered/betatorelease/proxy/translator/clientbound/ServerSpawnGlobalEntityTranslator.java
@@ -3,6 +3,7 @@ package com.github.dirtpowered.betatorelease.proxy.translator.clientbound;
 import com.github.dirtpowered.betaprotocollib.packet.Version_B1_7.data.V1_7_3ThunderboltPacketData;
 import com.github.dirtpowered.betatorelease.network.session.Session;
 import com.github.dirtpowered.betatorelease.proxy.translator.ModernToBetaHandler;
+import com.github.dirtpowered.betatorelease.utils.Utils;
 import com.github.steveice10.mc.protocol.data.game.entity.type.GlobalEntityType;
 import com.github.steveice10.mc.protocol.packet.ingame.server.entity.spawn.ServerSpawnGlobalEntityPacket;
 
@@ -10,13 +11,14 @@ public class ServerSpawnGlobalEntityTranslator implements ModernToBetaHandler<Se
 
     @Override
     public void translate(ServerSpawnGlobalEntityPacket packet, Session betaSession) {
-        int entityType = packet.getEntityId();
-        int x = (int) packet.getX();
-        int y = (int) packet.getY();
-        int z = (int) packet.getZ();
+        int entityId = packet.getEntityId();
+
+        int x = Utils.toAbsolutePos(packet.getX());
+        int y = Utils.toAbsolutePos(packet.getY());
+        int z = Utils.toAbsolutePos(packet.getZ());
 
         if (packet.getType() == GlobalEntityType.LIGHTNING_BOLT) {
-            betaSession.sendPacket(new V1_7_3ThunderboltPacketData(entityType, x, y, z, 1));
+            betaSession.sendPacket(new V1_7_3ThunderboltPacketData(entityId, x, y, z, 1));
         }
     }
 }
