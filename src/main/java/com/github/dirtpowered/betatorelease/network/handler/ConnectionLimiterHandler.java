46291e72584d2e78e26ff3158d5329783ae1278c
outofmem13 <nulln785@gmail.com>
2024-11-14 04:03:10 +0100

    opss

--- src/main/java/com/github/dirtpowered/betatorelease/network/handler/ConnectionLimiterHandler.java
+++ src/main/java/com/github/dirtpowered/betatorelease/network/handler/ConnectionLimiterHandler.java
@@ -1,30 +1,35 @@
 package com.github.dirtpowered.betatorelease.network.handler;
 
 import com.github.dirtpowered.betatorelease.Main;
+import com.google.common.cache.Cache;
+import com.google.common.cache.CacheBuilder;
 import io.netty.channel.ChannelHandler;
 import io.netty.channel.ChannelHandlerContext;
 import io.netty.channel.ChannelInboundHandlerAdapter;
 
 import java.net.InetSocketAddress;
-import java.util.HashMap;
-import java.util.Map;
+import java.util.concurrent.TimeUnit;
 
 @ChannelHandler.Sharable
 public class ConnectionLimiterHandler extends ChannelInboundHandlerAdapter {
-    private static final Map<String, Long> CONNECTIONS = new HashMap<>();
+    private static final Cache<String, Long> CONNECTIONS = CacheBuilder.newBuilder()
+            .expireAfterWrite(10, TimeUnit.SECONDS)
+            .build();
+
+    private static final long THROTTLE_TIME_MS = 350L;
 
     @Override
     public void channelActive(ChannelHandlerContext ctx) throws Exception {
         InetSocketAddress isa = (InetSocketAddress) ctx.channel().remoteAddress();
         String address = isa.getAddress().getHostAddress();
 
-        if (CONNECTIONS.containsKey(address)) {
-            if (System.currentTimeMillis() - CONNECTIONS.get(address) < 350L) {
-                CONNECTIONS.put(address, System.currentTimeMillis());
-                Main.LOGGER.info("Connection throttled: " + address);
-                ctx.close();
-                return;
-            }
+        Long lastConn = CONNECTIONS.getIfPresent(address);
+
+        if (lastConn != null && (System.currentTimeMillis() - lastConn < THROTTLE_TIME_MS)) {
+            CONNECTIONS.put(address, System.currentTimeMillis());
+            Main.LOGGER.info("Connection throttled: " + address);
+            ctx.close();
+            return;
         }
 
         CONNECTIONS.put(address, System.currentTimeMillis());
